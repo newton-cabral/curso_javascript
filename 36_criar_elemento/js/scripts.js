563e8238d7e218a3adadf0afe02297a6767d266b
newton-cabral <newton_cabral@yahoo.com>
2021-05-25 11:23:49 -0300

    Inserindo elemento
    adicionei alguns comentarios

--- 36_criar_elemento/js/scripts.js
+++ 36_criar_elemento/js/scripts.js
@@ -1,8 +1,8 @@
 // inserir o elemento no body
-var novoParagrafo = document.createElement("p");
-
+var novoParagrafo = document.createElement("p"); // criou a tag p
+// insere o conteudo no paragrafo
 var texto = document.createTextNode("Este é o conteúdo do paragráfo");
-
+// inserindo o texto
 novoParagrafo.appendChild(texto);
 
 console.log(novoParagrafo);
@@ -10,19 +10,19 @@ console.log(novoParagrafo);
 var body = document.querySelector("body");
 
 console.log(body);
-
+// inserindo no corpo do html
 body.appendChild(novoParagrafo);
 
 // inserir em um container
-
+// mapeou a div
 var container = document.getElementById("container");
 
 console.log(container);
-
+// cria o elemento 
 var el = document.createElement("span");
-
+// inserindo o texto no span
 el.appendChild(document.createTextNode("texto do span"));
 
 console.log(el);
-
+// inserindo dentro do container
 container.appendChild(el);

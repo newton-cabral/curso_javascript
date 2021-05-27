70ed4e3c579a476462a611341e868a307d44939b
newton-cabral <newton_cabral@yahoo.com>
2021-05-27 11:18:12 -0300

    finalizado o projeto

--- 50_projeto copy/js/scripts.js
+++ 50_projeto copy/js/scripts.js
@@ -1,15 +1,25 @@
 // função que adiciona tarefa
 
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  addTask();
+})
+
+//função que adiciona tarefa
 function addTask() {
   // titulo da tarefa
   const taskTitle = document.querySelector("#task-title").value;
 
-
   if (taskTitle) {
+    //clonar template
     const template = document.querySelector(".template");
+
     const newTask = template.cloneNode(true);
+
     // adiciona titulo
-    newTask.querySelector(".task-title").textcontent = taskTitle;
+    newTask.querySelector(".task-title").textContent = taskTitle;
 
     //remover as classes desnecessárias
 
@@ -19,24 +29,40 @@ function addTask() {
     // adicionar a tarefa na lista
 
     const list = document.querySelector("#task-list");
+
     list.appendChild(newTask);
 
-// limpa texto
-document.querySelector("#task-title").value = "";
+
+
+    // adicionar o evento remover
+
+    const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function () {
+      removeTask(this)
+    });
+
+    // adicionar evento de completar tarefa
+
+    const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function () {
+      completeTask(this);
+    })
+
+
+    // limpar texto
+    document.querySelector("#task-title").value = "";
 
 
   }
 }
 
+//função de remover tarefa
 
+function removeTask(task) {
+  task.parentNode.remove(true);
+}
 
-
-// evento adiciona tarefa
-
-const addBtn = document.querySelector("#add-btn");
-
-addBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  addTask();
-})
+//função de completara tarefa
+function completeTask(task) {
+  const taskToComplete = task.parentNode;
+  taskToComplete.classList.toggle("done");
+  // toggle serve para se o elemento tem a classe ele coloca se não tem ele remove
+}
